40de66624e0b6d7b68a4829e65148ac8df56cf71
Roman Romanov <xdrond@gmail.com>
2019-02-25 11:47:29 +0700

    Added checksum

--- Client.py
+++ Client.py
@@ -1,14 +1,24 @@
 import socket
 import pickle
+import hashlib
+from random import random
+
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
+# Данные к отправке
+data = random()
+data = str(data).encode('utf-8')
+# Контрольная сумма
+checksum = hashlib.md5(data).digest()
+print('data:', data)
+print('checksum: ', checksum)
+# Сериализация данных
+to_send = pickle.dumps({'data': data, 'checksum': checksum})
+print('Pickle data:', to_send)
+print("Size of data to send: ", to_send.__sizeof__())
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    while True:
-        usr_in = input('Data to send(S - to exit):')
-        if usr_in == 'S':
-            break
-        pickle_data = pickle.dumps(usr_in)
-        s.sendall(pickle_data)
+    s.sendall(to_send)
--- Server.py
+++ Server.py
@@ -1,19 +1,27 @@
 import socket
 import pickle
+import hashlib
+from random import random
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+print("Waiting for connect...")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
+    conn, address = s.accept()
     with conn:
-        print('Connected by', addr)
+        print('Connected by', address)
         while True:
-            data = conn.recv(1024)
-            if not data:
+            rec_data = pickle.loads(conn.recv(1024))
+            if not rec_data:
                 break
-            rec_data = pickle.loads(data)
-            print(rec_data)
-            #conn.sendall(data)
+if hashlib.md5(rec_data['data']).digest() == rec_data['checksum']:
+    print("Data is correct.")
+else:
+    print("Data is corrupted.")
+print("Number:", rec_data['data'])
+print("Checksum:", rec_data['checksum'])
+
+
