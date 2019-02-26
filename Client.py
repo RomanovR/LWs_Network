708126f3a3d914aeadf464e19d617d1b101a6a15
Roman Romanov <xdrond@gmail.com>
2019-02-26 14:44:29 +0700

    2/3 done

--- Client.py
+++ Client.py
@@ -3,22 +3,29 @@ import pickle
 import hashlib
 from random import random
 
-
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+PORT = 49001        # The port used by the server
 
-# Данные к отправке
-data = random()
-data = str(data).encode('utf-8')
-print('Data to send: ', data)
-# Контрольная сумма
-checksum = hashlib.md5(data).digest()
-# Сериализация данных
-to_send = pickle.dumps({'data': data, 'checksum': checksum})
 
-print('Connecting...')
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(to_send)
-    print('Sent.')
+def main():
+    # Данные к отправке
+    data = random()
+    print('Data to send: ', data)
+    data = str(data).encode('utf-8')
+    # Контрольная сумма
+    checksum = hashlib.md5(data).digest()
+    # Сериализация данных
+    to_send = pickle.dumps({'data': data, 'checksum': checksum})
 
+    print('Connecting...')
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((HOST, PORT))
+        except ConnectionRefusedError:
+            print('Connection refused.')
+        else:
+            s.sendall(to_send)
+            print('Sent.')
+
+
+main()
--- Server.py
+++ Server.py
@@ -1,32 +1,62 @@
+import sys
 import socket
 import pickle
 import hashlib
 from random import random
 
+# For the receiving socket.
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 49001  # Port to listen on (non-privileged ports are > 1023)
 
-print("Waiting for connect...")
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, address = s.accept()
-    with conn:
-        print('Connected by ', address)
-        x = 5
-        while True:
-            data_rec = conn.recv(1024)
-            if not data_rec:
-                break
-            try:
-                data = pickle.loads(data_rec)
-                if hashlib.md5(data['data']).digest() != data['checksum']:
-                    print("Data is corrupted.")
-                    
-                print("Number:", float(data['data']))
-            except pickle.UnpicklingError:
-                print("Received incorrect data.")
+#For the transmitting socket
+HOST_3 = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT_3 = 49002  # Port to listen on (non-privileged ports are > 1023)
 
 
+def main():
+    print("Waiting for connect...")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, address = s.accept()
+        with conn:
+            print('Connected by ', address)
+            while True:
+                data_rec = conn.recv(1024)
+                if not data_rec:
+                    break
+                try:
+                    data = pickle.loads(data_rec)
+                    if hashlib.md5(data['data']).digest() != data['checksum']:
+                        raise ValueError
+                except pickle.UnpicklingError:
+                    print("Received incorrect data.")
+                    sys.exit()
+                except ValueError:
+                    print("Incorrect checksum, data is corrupted.")
+                    sys.exit()
+                else:
+                    print("Number:", float(data['data']))
+
+                    # Data to send.
+                    data_add = random()
+                    print('Data to send: ', float(data['data']), ', ', data_add)
+                    data_add = str(data_add).encode('utf-8')
+
+                    checksum = hashlib.md5(data['data'] + data_add).digest()
+                    print(checksum)
+                    # Serialization data + checksum.
+                    to_send = pickle.dumps({'1': data['data'], '2': data_add, 'checksum': checksum})
+
+                    print('Connecting...')
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_to_third:
+                        try:
+                            s_to_third.connect((HOST, PORT))
+                        except ConnectionRefusedError:
+                            print('Connection refused.')
+                        else:
+                            s_to_third.sendall(to_send)
+                            print('Sent.')
 
 
+main()
