82a9dd7393dbf5ee495fe03a4bcd97d5256c2886
xdrond <xdrond@gmail.com>
2019-04-13 21:56:27 +0700

    Переверстать интерфейс и убрать передачу subject, Добавить предупреждение о кириллице Починить уведомление "Sending..." А в целом уже работает))0))0)

--- LW2/SMTP_Server.py
+++ LW2/SMTP_Server.py
@@ -1,29 +1,88 @@
 import socket
 import pickle
-#import smtplib, ssl
+import sys
+import smtplib, ssl
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 49001  # Port to listen on (non-privileged ports are > 1023)
+PORT = 49001  # Port to listen
 
-port = 465  # For SSL
-password = input("Type your password and press enter: ")
 
-print("Waiting for connect...")
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, address = s.accept()
-    with conn:
-        print('Connected by', address)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            try:
-                data = pickle.loads(data)
-            except pickle.UnpicklingError:
-                print("Received incorrect data.")
-            else:
-                print('To:', data['To'])
-                print('Theme:', data['Theme'])
-                print('Message:', data['Mail'])
+def getmessage():
+    print("Waiting for connect...")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, address = s.accept()
+        with conn:
+            print('Connected by', address)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                try:
+                    data = pickle.loads(data)
+                except pickle.UnpicklingError:
+                    print("Received incorrect data.")
+                    sys.exit()
+                else:
+                    print('To:', data['To'])
+                    print('Theme:', data['Theme'])
+                    print('Message:', data['Mail'])
+                    data = (data['To'], data['Theme'], data['Mail'])
+                    smtp_server = "smtp.gmail.com"
+                    port = 587  # For starttls
+                    sender_email = "xdrond.bot@gmail.com"
+                    password = "DcrqDHpvt9bBEpA"
+                    receiver_email = data[0]  # Enter receiver address
+                    message = data[2]
+
+                    # Create a secure SSL context
+                    context = ssl.create_default_context()
+
+                    # Try to log in to server and send email
+                    try:
+                        server = smtplib.SMTP(smtp_server, port)
+                        server.ehlo()  # Can be omitted
+                        server.starttls(context=context)  # Secure the connection
+                        server.ehlo()  # Can be omitted
+                        server.login(sender_email, password)
+                        server.sendmail(sender_email, receiver_email, message)
+                        print('Sent to gmail.')
+                    except Exception as e:
+                        # Print any error messages
+                        print(e)
+                    finally:
+                        server.quit()
+
+
+def sendingmail(data):
+    smtp_server = "smtp.gmail.com"
+    port = 587  # For starttls
+    sender_email = "xdrond.bot@gmail.com"
+    password = "DcrqDHpvt9bBEpA"
+    receiver_email = data[0]  # Enter receiver address
+    message = data[2]
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    # Try to log in to server and send email
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)  # Secure the connection
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+    except Exception as e:
+        # Print any error messages
+        print(e)
+    finally:
+        server.quit()
+
+
+def main():
+    getmessage()
+
+
+main()
